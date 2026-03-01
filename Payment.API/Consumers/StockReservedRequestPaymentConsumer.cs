f932e2b4f2cfbbdb6cfc769bfdba3a7b32227f7e
Osman Tonbul <osmantonbul2001@gmail.com>
2026-03-01 17:23:22 +0300

    test

---
+++ Payment.API/Consumers/StockReservedRequestPaymentConsumer.cs
@@ -0,0 +1,13 @@
+﻿using MassTransit;
+using Shared.Abstract;
+
+namespace Payment.API.Consumers
+{
+	public class StockReservedRequestPaymentConsumer : IConsumer<IStockReservedRequestPaymentEvent>
+	{
+		public Task Consume(ConsumeContext<IStockReservedRequestPaymentEvent> context)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
